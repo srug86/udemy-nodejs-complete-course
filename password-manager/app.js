f897624543e38aea05ffc963f7e275a135f16422
Sergio de la Rubia <srug86@gmail.com>
2017-01-10 00:08:04 +0100

    Add 'Getting Input From User' example.

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,39 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+function createAccount(account) {
+	var accounts = storage.getItemSync('accounts');
+
+	if (typeof accounts === 'undefined') {
+		accounts = [];
+	}
+
+	accounts.push(account);
+	storage.setItemSync('accounts', accounts);
+
+	return account;
+}
+
+function getAccount(accountName) {
+	var accounts = storage.getItemSync('accounts');
+	var matchedAccount;
+
+	accounts.forEach(function(account) {
+		if (account.name === accountName) {
+			matchedAccount = account;
+		}
+	});
+
+	return matchedAccount;
+}
+
+//createAccount({
+//	name: 'Facebook',
+//	username: 'someemail@gmail.com',
+//	password: 'Password123!'
+//});
+
+var facebookAccount = getAccount('Facebook');
+console.log(facebookAccount);
