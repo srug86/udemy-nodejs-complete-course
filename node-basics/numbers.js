370eedf18af19d3ba1f9800b7e5a5a30b8b76c8d
Sergio de la Rubia <srug86@gmail.com>
2017-01-09 21:14:23 +0100

    Add 'Numbers' example.

---
+++ node-basics/numbers.js
@@ -0,0 +1,27 @@
+var age = 30;
+var results;
+
+results = age + 6;
+console.log('age + 6 = ' + results);
+
+results = age - 1;
+console.log('age - 1 = ' + results);
+
+results = age * 3;
+console.log('age * 3 = ' + results);
+
+results = age / 6;
+console.log('age / 6 = ' + results);
+
+results = age % 5;
+console.log('age % 5 = ' + results);
+
+results--;
+console.log('minus 1: ' + results);
+
+var testOne = 12;
+var testTwo = 75;
+var testThree = 60;
+var averageGrade = (testOne + testTwo + testThree) / 3;
+
+console.log('Your average grade is: ' + averageGrade);
