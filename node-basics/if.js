0556016f43722bc51e267066c5ed5a40a153062a
Sergio de la Rubia <srug86@gmail.com>
2017-01-09 21:14:47 +0100

    Add 'If Statements' example.

---
+++ node-basics/if.js
@@ -0,0 +1,9 @@
+var name = 'Chet';
+
+if (name === 'Mike' || name === 'Jen') {
+	console.log('Hello friend!');
+} else if (name === 'Chet') {
+	console.log('Hey');
+} else {
+	console.log('Hey stranger!');
+}
